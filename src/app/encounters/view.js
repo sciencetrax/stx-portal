44313de2088742fa9db11b8ab2cffe0463946fad
Leslie Hanks <leslie@lesliehanks.com>
2013-09-29 02:10:09 -0500

    Adding support for updating encounters

--- src/app/encounters/view.js
+++ src/app/encounters/view.js
@@ -4,15 +4,25 @@
     angular.module('stx.encounters.view', [
             'ui.router'
         ])
-        .controller("stx.encounters.view.controller", ['$scope', '$state', '$stateParams',
-            function ($scope, $state, $stateParams) {
+        .controller("stx.encounters.view.controller", ['$scope', '$state', '$stateParams', 'SecurityService', 'ScheduledEncounter',
+            function ($scope, $state, $stateParams, SecurityService, ScheduledEncounter) {
                 $scope.$state = $state;
                 $scope.stateParams = $stateParams;
                 if ($state.current.name === 'encounters.view') {
                     $state.go('encounters.view.details');
                 }
+
+                ScheduledEncounter.query({
+                    customerId: SecurityService.authorizationContext.customerId,
+                    projectId: SecurityService.authorizationContext.subject.projects[0].projectId,
+                    siteId: SecurityService.authorizationContext.subject.projects[0].siteId,
+                    subjectId: SecurityService.authorizationContext.subject.id,
+                    $filter: 'IntervalId eq ' + $stateParams.intervalId //+ ' and EncounterId eq ' + $stateParams.encounterId
+                }, function (data) {
+                    $scope.encounter = data[0];
+                });
             }])
-        .controller("stx.encounters.view.details.controller", [
+        .controller("stx.encounters.view.reports.controller", [
             function () {
             }])
         .controller("stx.encounters.view.details.controller", ['$scope', '$state', '$stateParams', 'SecurityService', 'VariablePanelScript', 'DataEntryForm', 'ScheduledEncounter',
@@ -28,15 +38,6 @@
                     $stateParams.encounterId,
                     false);
 
-                ScheduledEncounter.query({
-                    customerId: SecurityService.authorizationContext.customerId,
-                    projectId: SecurityService.authorizationContext.subject.projects[0].projectId,
-                    siteId: SecurityService.authorizationContext.subject.projects[0].siteId,
-                    subjectId: SecurityService.authorizationContext.subject.id,
-                    $filter: 'IntervalId eq ' + $stateParams.intervalId //+ ' and EncounterId eq ' + $stateParams.encounterId
-                }, function (data) {
-                    $scope.encounter = data[0];
-                });
             }])
     ;
 }());
