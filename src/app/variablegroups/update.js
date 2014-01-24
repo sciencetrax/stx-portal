2904e43a800f080d905d9ec8e909febf488768f9
Leslie Hanks <leslie@lesliehanks.com>
2014-01-24 13:39:23 -0600

    UI Tweaks

--- src/app/variablegroups/update.js
+++ src/app/variablegroups/update.js
@@ -44,6 +44,17 @@
 							}
 						);
 					});
+				$scope.cancel = function () {
+					if($.dirtyState.isDirty()) {
+						bootbox.confirm("Page is dirty, are you sure you want to cancel?", function(success) {
+							if (success) {
+								$scope.back('home.index');
+							}
+						});
+						return;
+					}
+					$scope.back('home.index');
+				};
 				$scope.save = function () {
 					var form = $('form', dataEntryPanel);
 					var action = form.attr('action');
@@ -59,6 +70,17 @@
 							if ($('.validation-summary-errors li', dataEntryPanel).length === 0) {
 								$window.history.back();
 								$scope.$apply();
+							} else {
+								$('.IndentLevel1').parent().parent().addClass("Indent1").addClass("Indent");
+								$('.IndentLevel2').parent().parent().addClass("Indent2").addClass("Indent");
+								$('.IndentLevel3').parent().parent().addClass("Indent3").addClass("Indent");
+								$('.IndentLevel4').parent().parent().addClass("Indent4").addClass("Indent");
+
+								$(document).trigger('pageLoad');
+								if ($(".DataEntryPanel.ReadOnly").length == 1) {
+									$scope.readOnly = true;
+								}
+								$scope.ready = true;
 							}
 						}
 					});
